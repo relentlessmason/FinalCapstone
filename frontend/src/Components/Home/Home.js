3a73fe118b550e685c223b599c2b280b9fffcb74
Kala Harper <kala.n.harper@gmail.com>
2022-09-07 12:50:11 -0500

    quicklook calendar present but not finished

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,26 +1,32 @@
 import {Link} from 'react-router-dom'
+import QuickLook from './QuickLook';
+import Details from './Details';
+import {useState} from 'react';
+import './Home.css'
 
 function Home(props) {
+    const [showDetails, setShowDetails] = useState(false);
+    const [data, setData] = useState(null);
 
+    const showDetailsHandle = (dayStr) => {
+        setData(dayStr);
+        setShowDetails(true);
+    };
 
     return(
         <>
-    <div>
-    ID:
-    {"  "+props.user.id}
-    <br/>
-    USERNAME: 
-    {"  "+props.user.username}
-    </div>
-    <p>
-            There once was a llama named greg <br/>
-            who loved to chew on a peg <br/>
-            And then one night <br/>
-            his peg gave a cry <br/>
-            and greg a great fright <br/>
-            when turned down to find <br/>
-            that it wasn't wood but a leg
-    </p>
+            <div>
+            ID:
+            {"  "+props.user.id}
+            <br/>
+            USERNAME: 
+            {"  "+props.user.username}
+            </div>
+  
+    
+      <QuickLook showDetailsHandle={showDetailsHandle} />
+      <br />
+      {/* {showDetails && <Details data={data} />}    */}
     </>
     )
 }
