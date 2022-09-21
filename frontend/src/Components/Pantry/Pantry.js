f96f026ae90f7c037b2455d93eed09d2bac2e5f3
Edy <edysmith15@gmail.com>
2022-09-21 00:26:36 -0400

    pantry input box

--- frontend/src/Components/Pantry/Pantry.js
+++ frontend/src/Components/Pantry/Pantry.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { Control, LocalForm } from "react-redux-form";
+import { Label, Button, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./Pantry.css";
 
-export default function Pantry(){
+export default function Pantry(props){
     return(
-        <>
-        Pantry route is working
-        </>
+      <LocalForm
+      id="form"
+      onSubmit={(values)=> props.handlePostPantry(values)} 
+      >
+       
+        <div className="pantry">
+          <Row className="form-group">
+            <Label htmlFor="pantry" md={12}>
+              Pantry List
+            </Label>
+            <Col md={12} className="input-group">
+              <Control.textarea
+                model=".pantry"
+                id="pantry"
+                className="form-control"
+                type="textarea"
+                name="item"
+                placeholder="'Chicken, rice, milk...'"
+                rows="6"
+                
+                />
+
+            </Col>
+          </Row>
+          <Button
+              onClick={(e) => {
+                e.preventDefault();
+                //not sure how to gather multiple input fields and put them into db yet
+              }}
+              className="submitAR"
+              type="submit"
+            >
+              Add Ingredients
+            </Button>
+        </div>
+          
+      </LocalForm>
     )
 }
+// export default function Pantry(){
+//     return(
+//         <>
+//         Pantry route is working
+//         </>
+//     )
+// }
